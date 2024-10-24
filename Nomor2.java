8301925162a83e4686d8c9b6d616facb8aeddd7e
mfazhelolwhendra <fazelybumble1001@gmail.com>
2024-10-24 08:37:41 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -1,5 +1,58 @@
-public class Nomor2{
-    public static void main(String[] args){
+package Asesment;
+
+import java.util.Scanner;
+
+public class Ulangan1 {
+    public static void main(String[] args) {
+        Scanner scbd = new Scanner(System.in);
+        String Beliau, Mereka, Kita;
+        int tripKM;
+        
+        double Omset;
+        int totalTrip;
+        
+        System.out.println("Masukkan Driver [Beliau/Mereka/Kita]? ");
+        String namaDriver = scbd.nextLine();
+
+        if (Beliau.equals(namaDriver)) {
+            System.out.println("lama trip (km): ");
+            tripKM = scbd.nextInt();
+            
+            Omset = tripKM * 13000;
+            
+            
+    
+            
+
+            if (Mereka.equals(namaDriver)) {
+                System.out.println("lama trip (km): ");
+                tripKM = scbd.nextInt();
+
+            
+                Omset = tripKM * 13000;
+                
+                
+
+                if (Kita.equals(namaDriver)) {
+                    System.out.println("lama trip (km): ");
+                    tripKM = scbd.nextInt();
+                    
+                    Omset = tripKM * 13000;
+                    
+                    
+                    
+                }
+                
+            }
+        
+        }
+        totalTrip = tripKM ++;
+        System.out.println("Total trip yang dilakukan driver: ");
+
+
+
+
 
     }
+    
 }
